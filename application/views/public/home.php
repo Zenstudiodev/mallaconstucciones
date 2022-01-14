559a96918601782a569fc300c5cc2599209f3829
Latios-ws <csamayoa@zenstudiogt.com>
2022-01-14 12:48:19 -0600

    pagina frontal main ui

--- application/views/public/home.php
+++ application/views/public/home.php
@@ -11,3 +11,181 @@ $this->layout('public/public_master');
 ?>
 
 
+<?php $this->start('css_p') ?>
+<?php $this->stop() ?>
+
+<?php $this->start('page_content') ?>
+    <div id="banner_container">
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="/ui/public/images/slides/1.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="/ui/public/images/slides/2.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="/ui/public/images/slides/3.jpg" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade"
+                    data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade"
+                    data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="home_title text-uppercase"> Venta, Fabricación e Instalación <br/>
+                    <small class="text-muted text-lowercase">de Malla Galvanizada, Razor Ribbon, Privacinta, Euro-reja,
+                        Portones eléctricos.
+                    </small>
+                </h2>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md">
+                <div class="card" style="">
+                    <img src="/ui/public/images/fp_1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Malla Galvanizada</h5>
+                        <p class="card-text">La malla metálica está recubierta repetidamente con zinc para sellar la
+                            malla y protegerla del óxido y la corrosión</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md">
+                <div class="card" style="">
+                    <img src="/ui/public/images/fp_2.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Malla forrada en PVC</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the
+                            card's
+                            content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md">
+                <div class="card" style="">
+                    <img src="/ui/public/images/fp_3.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Reja Estilo Europeo</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the
+                            card's
+                            content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md">
+                <div class="card" style="">
+                    <img src="/ui/public/images/fp_4.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Razor Ribon</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the
+                            card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md">
+                <div class="card" style="">
+                    <img src="/ui/public/images/fp_5.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Barandas y pasamanos</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the
+                            card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="parallax_1h">
+        <h1 id="parallax_title"><span class="align-middle">Estamos listos para cumplir todas sus necesidades de seguridad</span>
+        </h1>
+    </div>
+    <div id="quality_container">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md">
+                    <div class="icon_container">
+                        <i class="fas fa-search"></i>
+                    </div>
+                    <h3 class="quality_t">Calidad</h3>
+                    <p>
+                        Some quick example text to build on the card title and make up the bulk of
+                    </p>
+                </div>
+                <div class="col-12 col-md">
+                    <div class="icon_container">
+                        <i class="fas fa-check-double"></i>
+                    </div>
+                    <h3 class="quality_t">Experiencia</h3>
+                    <p>
+                        Some quick example text to build on the card title and make up the bulk of
+                    </p>
+                </div>
+                <div class="col-12 col-md">
+                    <div class="icon_container">
+                        <i class="fas fa-shield-alt"></i>
+                    </div>
+                    <h3 class="quality_t">Seguridad</h3>
+                    <p>
+                        Some quick example text to build on the card title and make up the bulk of
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+<div id="contacto_container">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 ">
+                <h2 class="home_title text-uppercase"> Contacto </h2>
+                <form>
+                    <div class="mb-3">
+                        <label for="input_nombre" class="form-label">Nombre</label>
+                        <input type="text" class="form-control" id="input_nombre">
+                    </div>
+                    <div class="mb-3">
+                        <label for="input_email" class="form-label">Correo</label>
+                        <input type="email" class="form-control" id="input_email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="input_telefono" class="form-label">Teléfono</label>
+                        <div class="input-group">
+                            <div class="input-group-text">+502</div>
+                            <input type="tel" class="form-control" id="input_telefono" min-value="8" max-value="8">
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="input_mensaje" class="form-label">Información solicitada</label>
+                        <textarea  class="form-control" id="input_mensaje"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-success">Enviar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $this->stop() ?>
+<?php $this->start('js_p') ?>
+
+<?php $this->stop() ?>
--- application/views/public/public_master.php
+++ application/views/public/public_master.php
@@ -6,22 +6,149 @@
  * Time: 3:36 PM
  */
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="es">
-<head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="<?php echo base_url()?>/vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
-</head>
 <body>
-<h1>Hello, world!</h1>
+<div id="top_header">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <p>
+                    <a href="https://wa.me/50242734205" target="_blank"><i class="fab fa-whatsapp"></i> 42734205 |</a>
+                    <a href="tel:+50224351508"><i class="fas fa-phone"></i> 24351508 |</a>
+                    <a href="tel:+50222306151"><i class="fas fa-phone"></i> 22306151</a>
 
-<!-- jQuery first, then Tether, then Bootstrap JS. -->
-<script src="<?php echo base_url()?>/vendor/components/jquery/jquery.min.js" ></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-<script src="<?php echo base_url()?>/vendor/twbs/bootstrap/dist/js/bootstrap.min.js" ></script>
+                </p>
+            </div>
+            <div class="col">
+                <p>15 Av. 10-79 Zona 6 de Mixco, Guatemala</p>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+</div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light  sticky-top ">
+    <!--<div id="nav_container">-->
+        <div class="container-fluid" id="nav_container">
+            <a class="navbar-brand" href="#">
+                <img id="logo_main" src="/ui/public/images/logo.jpg" alt="Malla Construcciones de Guatemala">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                           data-bs-toggle="dropdown" aria-expanded="false">
+                            Productos
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Malla galvanizada</a></li>
+                            <li><a class="dropdown-item" href="#">Malla Forrada en PVC</a></li>
+                            <li><a class="dropdown-item" href="#">Reja Estilo Europeo</a></li>
+                            <li><a class="dropdown-item" href="#">Razor Ribbon</a></li>
+                            <li><a class="dropdown-item" href="#">Barandas y Pasamanos</a></li>
+
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nosotros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    <!--</div>-->
+</nav>
+
+<!-- Content Wrapper. Contains page content -->
+<?php echo $this->section('page_content') ?>
+<!-- /.content-wrapper -->
+<footer>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md">
+                <h4>Productos</h4>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"
+                    <a href="#">Malla Galvanizada</a></li>
+                    <li class="list-group-item"><a href="#">Malla forrada en PVC</a></li>
+                    <li class="list-group-item"><a href="#">Reja Estilo Europeo</a></li>
+                    <li class="list-group-item"><a href="#">Razor Ribon</a></li>
+                    <li class="list-group-item"><a href="#">Barandas y pasamanos</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-md">
+                <h4>Redes</h4>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><a href="#"><i class="fab fa-facebook-square"></i> Facebook</a></li>
+                    <li class="list-group-item"><a href="#"><i class="fab fa-instagram"></i> Instagram</a></li>
+                </ul>
+
+
+            </div>
+            <div class="col-12 col-md">
+                <h4>ubicacion</h4>
+                <p>15 Av. 10-79 Zona 6 de Mixco, Guatemala</p>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18836.57664287253!2d-90.59237281487589!3d14.667886894598524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85899ff2c22433a9%3A0x1c7b8ce759376473!2sCol%20San%20Francisco%2C%20Cdad.%20de%20Guatemala!5e1!3m2!1ses!2sgt!4v1642096086976!5m2!1ses!2sgt"
+                        style="border:0; width:100%; height:250px;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+            <div class="col">
+                <h4>Contato</h4>
+
+
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><a href="https://wa.me/50242734205" target="_blank"><i
+                                    class="fab fa-whatsapp"></i> 42734205</a></li>
+                    <li class="list-group-item"><a href="tel:+50224351508"><i class="fas fa-phone"></i> 24351508</a>
+                    </li>
+                    <li class="list-group-item"><a href="tel:+50222306151"><i class="fas fa-phone"></i> 22306151</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+-->
 </body>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;900&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="/ui/public/css/public.css" rel="stylesheet">
+    <title>Malla Constucciones de Guatemala</title>
+    <link rel="manifest" href="/manifest.json">
+    <script src="https://kit.fontawesome.com/fd7d02f666.js" crossorigin="anonymous"></script>
+</head>
 </html>
